a9befa8c9d8ba9f7afeb8b567bb7459f41834660
faruksiraje1469 <faruksiraje@gmail.com>
2025-02-27 20:12:49 +0600

    added file

---
+++ case.js
@@ -0,0 +1,16 @@
+const school = 'RAJ Uk Uttara Model School & College';
+
+console.log(school);
+console.log(school.toLocaleLowerCase());
+console.log(school.toLocaleUpperCase());
+
+
+
+const subject = 'Mathematics';
+const book = 'Mathematics';
+
+if (subject === book) {
+    console.log('i am reading a book on Mathematics');
+} else {
+    console.log('i am not reading a book on Mathematics');
+}
---
+++ string.js
@@ -0,0 +1,10 @@
+const country = 'Bangladesh';
+const city = "Dhaka";
+const division = `Dhaka Division`;
+// const thana = new String('Dhanmondi');
+
+
+ // string is immutable---not changeable
+ //array is mutable---changeable
+
+console.log(typeof country);             
